254f007d2e3078ace6256752007a380cdd60fa11
ProwainK <38719326+ProwainK@users.noreply.github.com>
2024-05-07 04:32:22 +0800

    Fix jacket miss

--- BGAnimations/ScreenGameplay underlay/Shared/SongInfoBarSingleUI.lua
+++ BGAnimations/ScreenGameplay underlay/Shared/SongInfoBarSingleUI.lua
@@ -5,7 +5,7 @@ local h = 22
 
 -- var for new song meter
 local SongMeterW = 136
-local SongMeterH = 3
+local SongMeterH = 12
 --local SongMeterX = _screen.cx + (player==PLAYER_1 and -1 or 1) * SL_WideScale(238, 288)
 local SongMeterX = _screen.cx + (1) * SL_WideScale(238, 288)
 
@@ -16,7 +16,7 @@ local SongMeterX = _screen.cx + (1) * SL_WideScale(238, 288)
 -- Song Completion Meter
 t[#t+1] = Def.ActorFrame{
 	Name="SongMeter",
-	InitCommand=function(self) self:y(_screen.cy+36) end,
+	InitCommand=function(self) self:y(_screen.cy+38) end,
 
 	-- border
 	Def.Quad{ InitCommand=function(self) self:xy(SongMeterX, 12):zoomto(SongMeterW+4, SongMeterH+4) end },
@@ -28,7 +28,7 @@ t[#t+1] = Def.ActorFrame{
 		Stream=Def.Quad({
       InitCommand=function(self)
         self:x(SongMeterX-68):horizalign("left"); -- 76 - 4 * 2 = 68?
-        self:y(12):zoomy(3):diffuse(GetCurrentColor(true));
+        self:y(12):zoomy(SongMeterH):diffuse(GetCurrentColor(true));
       end
     })
 	},
@@ -36,7 +36,7 @@ t[#t+1] = Def.ActorFrame{
 	-- Song Title
 	LoadFont("Common Normal")..{
 		Name="SongTitle",
-		InitCommand=function(self) self:xy(SongMeterX, 32):zoom(0.8):shadowlength(0.6):maxwidth(_screen.w/2.5 - 10) end,
+		InitCommand=function(self) self:xy(SongMeterX, 38):zoom(0.8):shadowlength(0.6):maxwidth(_screen.w/2.5 - 10) end,
 		CurrentSongChangedMessageCommand=function(self)
 
         --if P2_mods.NoteFieldOffsetY < -35 then
@@ -54,7 +54,7 @@ t[#t+1] = Def.ActorFrame{
 -- Song Artist
 t[#t+1] = LoadFont("Common Normal")..{
 		Name="SongArtist",
-		InitCommand=function(self) self:xy(SongMeterX, _screen.cy+88):zoom(0.8):shadowlength(0.6):maxwidth(_screen.w/2.5 - 10) end,
+		InitCommand=function(self) self:xy(SongMeterX, _screen.cy+98):zoom(0.8):shadowlength(0.6):maxwidth(_screen.w/2.5 - 10) end,
 		CurrentSongChangedMessageCommand=function(self)
 			local song = GAMESTATE:GetCurrentSong()
 			self:settext( song:GetDisplayArtist() or "" )
@@ -64,22 +64,22 @@ t[#t+1] = LoadFont("Common Normal")..{
 -- Song Jacket
 t[#t+1] = Def.ActorFrame{
 		Name="SongJacket",
-		InitCommand=function(self) self:xy(SongMeterX, _screen.cy-24):shadowlength(0.6) end,
+		InitCommand=function(self) self:xy(SongMeterX, _screen.cy-22):shadowlength(0.6) end,
 
 	-- border
 	Def.Quad{ InitCommand=function(self) self:zoomto(SongMeterW+4, SongMeterW+4) end },
-	--Def.Quad{ InitCommand=function(self) self:zoomto(SongMeterW, SongMeterW):diffuse(0,0,0,1) end },
+	Def.Quad{ InitCommand=function(self) self:zoomto(SongMeterW, SongMeterW):diffuse(0,0,0,1) end },
 
 	Def.Sprite {
 		OnCommand=function (self)
-      local jacket_size = SongMeterW;
+      local jacket_size = SongMeterW-6;
 			local course = GAMESTATE:GetCurrentCourse();
 			if GAMESTATE:IsCourseMode() then
 				if course:GetBackgroundPath() then
 					self:Load(course:GetBackgroundPath())
 					self:setsize(jacket_size, jacket_size);
 				else
-					self:Load(THEME:GetPathG("", "Common fallback jacket"));
+						self:Load(THEME:GetPathG("", "_VisualStyles/Hearts/TitleMenu (doubleres).png"));
 					self:setsize(jacket_size, jacket_size);
 				end;
 			else
@@ -92,7 +92,7 @@ t[#t+1] = Def.ActorFrame{
 						self:LoadFromSongBackground(GAMESTATE:GetCurrentSong());
 						self:setsize(jacket_size, jacket_size);
 					else
-						self:Load(THEME:GetPathG("","Common fallback jacket"));
+						self:Load(THEME:GetPathG("", "_VisualStyles/Hearts/TitleMenu (doubleres).png"));
 						self:setsize(jacket_size, jacket_size);
 					end;
 				else
